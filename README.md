4c690ba27e5c6be594b637e2a3e3408971a7cf3f
gokhangunduz <mail@gokhangunduz.com.tr>
2022-06-23 23:33:04 +0300

    Update main.py and add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Python Net Speed Test
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from speedtest import Speedtest
 print("Test working now...")
 net = Speedtest()
-
 print("Download Speed: " + str(round(net.download()/1000000,2)) + "Mbps")
 print("Upload Speed: " + str(round(net.upload()/1000000,2)) + "Mbps")
 print("Finish!")
