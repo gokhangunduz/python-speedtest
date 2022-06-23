e52f47fa70995ebac10c12b54c76f768169ec734
gokhangunduz <mail@gokhangunduz.com.tr>
2022-06-23 23:30:02 +0300

    Add upload test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from speedtest import Speedtest
-
+print("Test working now...")
 net = Speedtest()
 
 print("Download Speed: " + str(round(net.download()/1000000,2)) + "Mbps")
+print("Upload Speed: " + str(round(net.upload()/1000000,2)) + "Mbps")
+print("Finish!")
